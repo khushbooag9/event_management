c7f1398d2aace3b24b09969d0bb90714f2840a8c
khushbooag9 <khushiag565@gmail.com>
2025-07-20 18:48:32 +0530

    Done

--- api/index.js
+++ api/index.js
@@ -28,10 +28,7 @@ app.use(cors({
     origin: true,
 }));
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error(err));
 
