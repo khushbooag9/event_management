0a778ae0233fac8e4e75cbbda40aaf4bc942c028
khushbooag9 <khushiag565@gmail.com>
2025-07-20 18:31:22 +0530

    Done

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ app.use(cors({
     origin: true,
 }));
 
-mongoose.connect(process.env.MONGOURI, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
